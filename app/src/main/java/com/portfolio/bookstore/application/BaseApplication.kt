c588a866fbf09af973457d72d2a9de97a88abaff
= <dastageerb44@gmail.com>
2022-12-05 12:59:41 -0500

    hilt setup

---
+++ app/src/main/java/com/portfolio/bookstore/application/BaseApplication.kt
@@ -0,0 +1,10 @@
+package com.portfolio.bookstore.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+
+}

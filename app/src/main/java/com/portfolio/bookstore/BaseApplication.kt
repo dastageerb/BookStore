ccc132fbbce6402e104314ae3de5fa8a012e899e
= <dastageerb44@gmail.com>
2022-12-25 11:44:01 -0500

    crashlaytics removed

--- app/src/main/java/com/portfolio/bookstore/BaseApplication.kt
+++ app/src/main/java/com/portfolio/bookstore/BaseApplication.kt
@@ -1,13 +1,6 @@
 package com.portfolio.bookstore
 
 import android.app.Application
-import com.microsoft.appcenter.AppCenter
-import com.microsoft.appcenter.analytics.Analytics
-import com.microsoft.appcenter.crashes.Crashes
 
 class BaseApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        AppCenter.start(this, BuildConfig.AppCenterSecretKey, Analytics::class.java, Crashes::class.java)
-    }
 }

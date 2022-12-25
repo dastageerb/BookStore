4a0d38804b4cced87ca82db1f05538e1d7991cf0
= <dastageerb44@gmail.com>
2022-12-25 12:08:48 -0500

    errors resolved

--- app/src/main/java/com/portfolio/bookstore/MainActivity.kt
+++ app/src/main/java/com/portfolio/bookstore/MainActivity.kt
@@ -3,16 +3,6 @@ package com.portfolio.bookstore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.microsoft.appcenter.AppCenter
-import com.microsoft.appcenter.analytics.Analytics
-import com.microsoft.appcenter.crashes.Crashes
 import com.portfolio.bookstore.ui.theme.BookStoreTheme
 
 

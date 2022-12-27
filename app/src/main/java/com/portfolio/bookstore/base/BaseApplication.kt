cd5d3509070d39562d31168974caf01c87907f48
= <dastageerb44@gmail.com>
2022-12-27 13:19:13 -0500

    dependency injection setup

--- app/src/main/java/com/portfolio/bookstore/base/BaseApplication.kt
+++ app/src/main/java/com/portfolio/bookstore/base/BaseApplication.kt
@@ -2,8 +2,10 @@ package com.portfolio.bookstore.base
 
 import android.app.Application
 import com.portfolio.bookstore.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
